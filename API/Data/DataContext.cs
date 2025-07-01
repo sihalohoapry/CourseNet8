015e1b001451383199d878de161144661319f21c
Apry Sihaloho <madonaapry@gmail.com>
2025-07-01 14:39:30 +0700

    End Section 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,10 @@
+using System;
+using API.Entitties;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<AppUser> Users { get; set; }
+}
