0bc226d3b617b49a7547961432fa52ed1a9cbdc3
Apry Sihaloho <madonaapry@gmail.com>
2025-07-09 22:06:19 +0700

    End section 3

---
+++ client/src/app/app.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App implements OnInit {
+  http = inject(HttpClient);
+  protected title = 'DatingApp';
+  users: any;
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: (response) => (this.users = response),
+      error: (error) => console.log(error),
+      complete: () => console.log('Request has complate'),
+    });
+  }
+}
